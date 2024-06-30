4a1d0ac89c4c9102691fe196668c3cb08bdb13a7
surajguduru <guduru.23bcs10012@ms.sst.scaler.com>
2024-06-30 13:50:54 +0530

    first commit

---
+++ BufferedImage.java
@@ -0,0 +1,16 @@
+
+public class BufferedImage {
+
+    public int getHeight() {
+        return 0;
+    }
+
+    public char[] getRGB(int j, int i) {
+        return null;
+    }
+
+    public int getWidth() {
+        return 0;
+    }
+
+}
